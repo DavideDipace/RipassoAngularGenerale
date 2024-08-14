228caff8030977962a4ab04b55570c5cce56d72a
DavideDipace <dipace.davide@iisgalvanimi.edu.it>
2024-08-14 12:34:18 +0000

    punto quattro fatto

--- Dipace-Calcolatrice/src/app/app.module.ts
+++ Dipace-Calcolatrice/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RisultatoComponent } from './risultato/risultato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RisultatoComponent
   ],
   imports: [
     BrowserModule
---
+++ Dipace-Calcolatrice/src/app/risultato/risultato.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-risultato',
+  templateUrl: './risultato.component.html',
+  styleUrls: ['./risultato.component.css']
+})
+export class RisultatoComponent {
+
+}
