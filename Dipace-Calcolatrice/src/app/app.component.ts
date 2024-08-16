5535d5eb195a8cf8158b3e2bdd3cf524ce5c176a
DavideDipace <dipace.davide@iisgalvanimi.edu.it>
2024-08-16 15:15:28 +0000

    tentativo punto 5

--- Dipace-Calcolatrice/src/app/app.component.ts
+++ Dipace-Calcolatrice/src/app/app.component.ts
@@ -7,24 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dipace-Calcolatrice';
- 
+  myRisultato;
+
+  constructor (){
+    this.myRisultato = {
+      addizione(num1: HTMLInputElement, num2: HTMLInputElement): boolean {
+        console.log(num1.valueAsNumber + num2.valueAsNumber);
+        return false;
+        },
+      
+        sottrazione(num1: HTMLInputElement, num2: HTMLInputElement): boolean {
+        console.log(num1.valueAsNumber - num2.valueAsNumber);
+        return false;
+        },
+        
+        divisione(num1: HTMLInputElement, num2: HTMLInputElement): boolean {
+        console.log(num1.valueAsNumber / num2.valueAsNumber);
+        return false;
+        },
+        
+        moltiplicazione(num1: HTMLInputElement, num2: HTMLInputElement): boolean {
+        console.log(num1.valueAsNumber * num2.valueAsNumber);
+        return false;
+        }
+    }
+  }
   addizione(num1: HTMLInputElement, num2: HTMLInputElement): boolean {
-  console.log(num1.valueAsNumber + num2.valueAsNumber);
+  //console.log(num1.valueAsNumber + num2.valueAsNumber);
   return false;
   }
 
   sottrazione(num1: HTMLInputElement, num2: HTMLInputElement): boolean {
-  console.log(num1.valueAsNumber - num2.valueAsNumber);
+  //console.log(num1.valueAsNumber - num2.valueAsNumber);
   return false;
   }
   
   divisione(num1: HTMLInputElement, num2: HTMLInputElement): boolean {
-  console.log(num1.valueAsNumber / num2.valueAsNumber);
+  //console.log(num1.valueAsNumber / num2.valueAsNumber);
   return false;
   }
   
   moltiplicazione(num1: HTMLInputElement, num2: HTMLInputElement): boolean {
-  console.log(num1.valueAsNumber * num2.valueAsNumber);
+  //console.log(num1.valueAsNumber * num2.valueAsNumber);
   return false;
   }
   ngOnInit() {}
--- Dipace-Calcolatrice/src/app/risultato/risultato.component.ts
+++ Dipace-Calcolatrice/src/app/risultato/risultato.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-risultato',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./risultato.component.css']
 })
 export class RisultatoComponent {
-    
+    @Input() risultato: any;
+
+    constructor() {
+
+    }
+    ngOnInit() {}
 }
